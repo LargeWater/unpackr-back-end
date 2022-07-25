fa12066167cd04b9e22fbbe5d00aa7370b64f2c7
Nicholas Bailey <nickcyg1992@gmail.com>
2022-07-25 16:18:06 -0500

    improvements

--- controllers/lectures.js
+++ controllers/lectures.js
@@ -1,9 +1,17 @@
 import { Lecture } from "../models/lecture.js"
+import { Profile } from "../models/profile.js"
 
 function create(req, res) {
   req.body.author = req.user.profile
   Lecture.create(req.body)
-  .then(lecture => res.json(lecture))
+  .then(lecture => 
+    Profile.findById(req.user.profile._id)
+    .then(profile => {
+      profile.lectures.push(lecture._id)
+      profile.save()
+      .then(() => res.json(lecture))
+    })
+  )
   .catch(err => {
     console.log(err)
     res.status(500).json({err: err.errmsg})
